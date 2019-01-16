85940c7dda9c3bce9448ac8deccf70d0306518d2
SheenaDub <sheenadavitt@gmail.com>
2019-01-16 20:16:27 +0000

    design fixes, better game logic and more

--- script.js
+++ script.js
@@ -3,36 +3,30 @@
 
 
 // to do
-// 1. disable stick and hit buttons if player has not yet clicked 'deal'
-// 2. fix div background so it fills out...
-// 3. fix game logic when it's dealer's turn
-// 4. add 'notification' div to put updates, instead of alerts
-// 5. panel for play buttons
+// 1. add small margin to extra cards
 // 6. add 'title' div at top of page
 // 7. logic to make ace worth 1 or 11, instead of just 11
+// fix time delay when dealer is dealing out its cards..
 
 
 
-var gameOn, allScores, currentPlayer, cardVal, cardNum, suitNum, suit, alreadyPlayed, cardName, hidden, hiddenCardScore, dealerCard1, dealerCard2, playerCard1, playerCard2;
+var gameOn, allScores, currentPlayer, cardVal, cardNum, suitNum, suit, alreadyPlayed, cardName, hiddenCardScore, dealerCard1, dealerCard2, playerCard1, playerCard2;
 
-
-var cardSlotNo = 2;
 var dealButton = document.querySelector('.playCard');
 var dealerExtracards=0;
 var playerExtracards=0;
 var dealerSlotNo = 0;
 var playerSlotNo = 0;
+var dealClicked = false;
+var hiddenCardShown = false;
 
 
 initialise();
 
 
-
-
 // attach anonymous function to playCard button
 document.querySelector('.playCard').addEventListener('click', function(){
-
-
+dealClicked=true;    
 if (gameOn){
     
 // pick four random cards to start        
@@ -58,8 +52,7 @@ document.getElementById('card1-0').src = "cards/" + dealerCard1 + ".svg";
 document.getElementById('card2-0').src = "cards/upturned.svg";    
 // display the cards in divs 'card1-' and 'card2-'
 document.getElementById('card1-1').src = "cards/" + playerCard1 + ".svg";
-document.getElementById('card2-1').src = "cards/" + playerCard2 + ".svg";
-    
+document.getElementById('card2-1').src = "cards/" + playerCard2 + ".svg";  
 
 // add card values to total score
 var score1 = scorer(cardNum1);
@@ -68,82 +61,79 @@ hiddenCardScore = scorer(cardNum2);
 allScores[0]+=score1;
 var score2 = scorer(cardNum3) + scorer(cardNum4);
 allScores[1]+=score2;
-   
     
 // display score
 document.querySelector('#score-0').textContent = allScores[0];
-document.querySelector('#score-1').textContent = allScores[1];
-
-    
+document.querySelector('#score-1').textContent = allScores[1]; 
 dealButton.disabled = true;   
-    
+// add hidden card to dealer's score, but don't display it yet
+allScores[0]+=hiddenCardScore;  
     
 if (score2===21 && (score1 + hiddenCardScore)!==21){    
-alert("You win!!! Natural blackjack");
+    document.querySelector('.notifications').textContent = "you win. Natural blackjack";
+//alert("You win!!! Natural blackjack");
     showDealerCard();
     gameOn=false; 
-}    
-    
+}     
 else if  (score2===21 && (score1 + hiddenCardScore)===21){
- alert("Draw! You and dealer get Natural blackjack");
+    document.querySelector('.notifications').textContent = "Draw! You and dealer get Natural blackjack";
+ //alert("Draw! You and dealer get Natural blackjack");
     showDealerCard();
     gameOn=false;
+} 
+
+else if (score2>21){
+    document.querySelector('.notifications').textContent = "Bust - over 21";
+    showDealerCard();
+    gameOn=false;
+    
 }
     
-       
-}    
     
-    
+}     
 })
 
 
-    
-
 function displayNewCard(card){
-    
+    var slot
+    var playerDiv
     if (currentPlayer===1){
         playerSlotNo++
-    var playerDiv = document.getElementById('p-1');
+    playerDiv = document.getElementById('p-1');
     currentPlayer === 0 ? dealerExtracards++ : playerExtracards++;
-    var slot = document.createElement('img');
+    slot = document.createElement('img');
     slot.id = 'playerCardSlot' + playerSlotNo; 
-    slot.style.width = "100px";
+    slot.style.width = '10%';  
     slot.class = 'newCardImage'; 
     playerDiv.appendChild(slot);
+        
     document.getElementById('playerCardSlot' + playerSlotNo).src = "cards/" + card + ".svg";  }
     
-    else{
-        
-      dealerSlotNo++
-    var playerDiv = document.getElementById('p-0');
+    else{  
+    dealerSlotNo++
+    playerDiv = document.getElementById('p-0');
     currentPlayer === 0 ? dealerExtracards++ : playerExtracards++;
-    var slot = document.createElement('img');
+    slot = document.createElement('img');
     slot.id = 'dealerCardSlot' + dealerSlotNo; 
-    slot.style.width = "100px";
+    slot.style.width = '10%';  
     slot.class = 'newCardImage'; 
     playerDiv.appendChild(slot);
-    document.getElementById('dealerCardSlot' + dealerSlotNo).src = "cards/" + card + ".svg";   
-        
-        
-    }
-    
-    
-    
-    
-    
-}
+    document.getElementById('dealerCardSlot' + dealerSlotNo).src = "cards/" + card + ".svg";  }
+} // end display new card function
 
 
 document.querySelector('.hit').addEventListener('click', hit)
+
 function hit(){
-    
-    if (gameOn){   
-// pick new card        
+    if (gameOn && dealClicked){   
+        
+if (currentPlayer===0){wait(750);}        
+        
+// pick new card and display       
 var newCard = pickUnplayedCard();
 var cardNum = newCard[0]; 
 var card = newCard[1];
-console.log("new card is " + card);
-     
+              
 displayNewCard(card);       
         
 // add card values to total score
@@ -155,91 +145,108 @@ document.querySelector('#score-' + currentPlayer).textContent = allScores[curren
 var thisScore =allScores[currentPlayer];
         
 if (thisScore===21)
-{
-    alert("you win!!! inside hit function");
+{ 
+    showDealerCard();
+    if (allScores[0]!==21){
+      document.querySelector('.notifications').textContent = "You win - 21 points!";
+    gameOn=false;}
+ 
+    else if (allScores[1]!==21){ document.querySelector('.notifications').textContent = "You lose. Dealer gets 21!";
+    gameOn=false;}
+ 
+    else{ 
+        document.querySelector('.notifications').textContent = "Draw! You and dealer get 21.";
+    gameOn=false;}   
 }
-   
+        
 else if (thisScore>21)       
-{alert("you are bust!!! inside hit function");
- gameOn=false;
-}
-        
-}    
+{ 
+showDealerCard();
+ var winner = currentPlayer===0? "Dealer is bust, You win" : "You are bust! Dealer wins"; 
+ document.querySelector('.notifications').textContent = winner;
+ gameOn=false;}   
     
+    }
     
-}
+} // end hit function
 
-// shows hidden dealer card and adds hidden card value to dealer score
+// shows hidden dealer card and dealers score with hidden card value added
 function showDealerCard(){ 
-document.getElementById('card2-0').src = "cards/" + dealerCard2 + ".svg";
-allScores[0]+=hiddenCardScore;    
-document.querySelector('#score-0').textContent = allScores[0];   
+document.getElementById('card2-0').src = "cards/" + dealerCard2 + ".svg";    
+document.querySelector('#score-0').textContent = allScores[0];
 }
 
+document.querySelector('.stick').addEventListener('click', stick);
+          
 
-document.querySelector('.stick').addEventListener('click', function(){
-     
-    if (gameOn){ 
-        
+
+function stick(){ 
+    
+if (gameOn && dealClicked){     
+    
 // dealer is now the current player        
 currentPlayer=0;        
-
 // change color to show it's dealer's turn    
 document.querySelector('.player-0').classList.toggle('current');
-document.querySelector('.player-1').classList.toggle('current');
-    
-var playerFinal = allScores[1];
-    
-setTimeout(function(){
-
+document.querySelector('.player-1').classList.toggle('current'); 
+var playerFinal = allScores[1]; 
 showDealerCard();
-
-if (allScores[0]===21){alert("dealer wins!!!");
+    
+    
+if (allScores[0]===21){
+    
+     document.querySelector('.notifications').textContent = "Dealer wins!!!";
                    gameOn=false;}
     
 while (allScores[0]<21){
     if  (allScores[0]>16){
-    alert("dealer holds here...!");
-    if(allScores[0]>playerFinal) {alert("dealer wins, has more points than you!");} 
-    else if  (allScores[0]<playerFinal){alert("you beat dealer!");}
-    else{alert("it's a tie!");}    
-    gameOn=false;
-    break;}
-       
-else 
-    // then dealer must choose another card
-{alert("dealer hits here...");
-    hit();
-    if (allScores[0]>21){
-    alert("Dealer is bust! You Win!");
+         document.querySelector('.notifications').textContent = "Dealer holds here!";
+        
+    if(allScores[0]>playerFinal) {
+        document.querySelector('.notifications').textContent = "Dealer wins!";
+    gameOn=false;} 
+    else if  (allScores[0]<playerFinal){  
+    document.querySelector('.notifications').textContent = "you beat dealer!";
+    gameOn=false;}
+    else{document.querySelector('.notifications').textContent = "It's a tie!";}    
     gameOn=false;
     break;
-}
-    check21(allScores[0])? alert("dealer wins after this hit"): alert("dealer does not win after this hit");}
     
-}
-
-}, 750);  
+    } // end if statement    
+    
+else 
+// then dealer must choose another card
+{   
+document.querySelector('.notifications').textContent = "Dealer hits here";
+    hit();
+   
+} // end else   
+          
         
-       
+            
+} // end while loop   
     
-}
+    
+}// end ' if game on' block
+    
+    
+    
+    
+}// end stick function
 
-    
-     
-    
-    
-}) 
-
-
+ function wait(ms) {
+    var start = Date.now(),
+        now = start;
+    while (now - start < ms) {
+      now = Date.now();
+    }
+}     
 
 // add initialise function to newGame button
 document.querySelector('.newGame').addEventListener('click', initialise)
 
 // pick a random card
 function pickOneCard(){
-    
-
 cardNum = Math.floor(Math.random()*13) + 2;
 suitNum = Math.floor(Math.random()*4) + 1;   
 if (suitNum ===1){
@@ -249,18 +256,14 @@ else if(suitNum ===2){
 else if(suitNum ===3){
     suit='d';}
 else {
-    suit='c';} 
-    
-cardName = cardNum + suit; 
-    
-return [cardNum, cardName];
-   
+    suit='c';}    
+cardName = cardNum + suit;    
+return [cardNum, cardName]; 
 }
 
 function pickUnplayedCard(){
-    
     if (alreadyPlayed.length===52){
-        alert("no more cards!");
+        document.querySelector('.notifications').textContent = "No more cards!";
         gameOn=false;
         return;
     }
@@ -274,15 +277,8 @@ function pickUnplayedCard(){
     } while (played);
         
     alreadyPlayed.push(cardEl[1]);
-    
     return cardEl;    
-    
-    
-
 }
-
-
-
 
 // work out and return value of card 
 function scorer (cardNum){
@@ -299,11 +295,8 @@ function checkPlayed(card){
     var played = (alreadyPlayed.indexOf(card) ===-1) ? false : true;
     return played;  }
 
-function check21(num){ return num===1 ? true : false; }
-
 // set/reset all scores to zero 
 function initialise(){
-    
 alreadyPlayed=[];
 allScores=[0,0];
 gameOn=true;
@@ -317,7 +310,10 @@ document.getElementById('card2-1').src = "";
 document.querySelector('.player-1').classList.remove('current'); 
 document.querySelector('.player-0').classList.remove('current');
 document.querySelector('.player-1').classList.add('current');
+document.querySelector('.notifications').textContent = "";
 dealButton.disabled = false; 
+dealClicked = false;
+
    
 if (dealerExtracards>0 || playerExtracards>0){
     removeCards();
@@ -332,28 +328,21 @@ currentPlayer=1;
 }// end initialise
 
 function removeCards(){
- 
     
 if (dealerExtracards>0)    
 {
     for (var i=1; i<= dealerExtracards; i++){
     var dealerDiv = document.getElementById('p-0');
     var element1 = document.getElementById('dealerCardSlot' + i);
-    console.log(element1);
-    dealerDiv.removeChild(element1);  }  
-    
+    dealerDiv.removeChild(element1);  }    
 }
- 
     
 if (playerExtracards>0)    
 {
     for (var j=1; j<= playerExtracards; j++){
     var playerDiv = document.getElementById('p-1');
     var element2 = document.getElementById('playerCardSlot' + j);
-    console.log(element2);
     playerDiv.removeChild(element2);  }  
-    
 }    
-    
        
-} // end remove function
+}  // end remove function
